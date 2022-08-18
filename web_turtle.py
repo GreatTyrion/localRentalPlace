55411cff34cfa1586a4ff5911cfafaf3915a7569
Hongyuan Qiu <hongyuan@MacBook-Pro.local>
2022-08-18 09:55:30 -0230

    Updated on 08/18/2022

--- web_turtle.py
+++ web_turtle.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
     }
 
     begin_time = datetime.now()
-    for num in range(1, 7):
+    for num in range(1, 10):
         print(f"Working turtle {num} is about to scrape")
         web_scraper(num)
 
